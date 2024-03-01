62e7dfcdefd0dd5a29e7aff534350fbdae58effe
BartlomiejPPG <bartlomiejnedzi2006@gmail.com>
2024-03-01 13:34:32 +0100

    add files

---
+++ Zad5_MVC/Migrations/20240229115615_Baza.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Zad5_MVC.Migrations
+{
+    public partial class Baza : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Klienci",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PESEL = table.Column<long>(type: "bigint", nullable: false),
+                    BirthYear = table.Column<int>(type: "int", nullable: false),
+                    Płeć = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Klienci", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Klienci");
+        }
+    }
+}
